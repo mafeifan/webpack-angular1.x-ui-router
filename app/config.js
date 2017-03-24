3e2e671d5519fd0f90cb1bc7928127446ef67e5e
finley <maf@shinetechchina.com>
2017-03-24 13:19:26 +0800

    update

--- app/config.js
+++ app/config.js
@@ -24,40 +24,32 @@ export default (ngModule, Angular) => {
             template: require('./page3/page3.html'),
             controller: 'Page3Controller',
             controllerAs: 'test'
+        }).state('page4', {
+            url: '/page4',
+            templateProvider: ['$q', function ($q) {
+                let deferred = $q.defer();
+                require.ensure(['./page4/page4.html'], function () {
+                    let template = require('./page4/page4.html');
+                    deferred.resolve(template);
+                });
+                return deferred.promise;
+            }],
+            controller: 'Page4Controller',
+            controllerAs: 'test',
+            resolve: {
+                foo: ['$q', '$ocLazyLoad', function ($q, $ocLazyLoad) {
+                    let deferred = $q.defer();
+                    require.ensure([], function () {
+                        let module = require('./page4/page4Module.js')(Angular);
+                        $ocLazyLoad.load({
+                            name: 'page4App'
+                        });
+                        deferred.resolve(module);
+                    });
+
+                    return deferred.promise;
+                }]
+            }
         });
     }]);
-    if(1==1){
-        ngModule.config(['$stateProvider', '$locationProvider', '$urlRouterProvider', function ($stateProvider, $locationProvider, $urlRouterProvider) {
-            $stateProvider.state('page4', {
-                url: '/page4',
-                templateProvider: ['$q', function ($q) {
-                    let deferred = $q.defer();
-                    require.ensure(['./page4/page4.html'], function () {
-                        let template = require('./page4/page4.html');
-                        deferred.resolve(template);
-                    });
-                    return deferred.promise;
-                }],
-                controller: 'Page4Controller',
-                controllerAs: 'test',
-                resolve: {
-                    foo: ['$q', '$ocLazyLoad', function ($q, $ocLazyLoad) {
-                        let deferred = $q.defer();
-                        require.ensure([], function () {
-                            let module = require('./page4/page4Module.js')(Angular);
-                            $ocLazyLoad.load({
-                                name: 'page4App'
-                            });
-                            deferred.resolve(module);
-                        });
-
-                        return deferred.promise;
-                    }]
-                }
-            });
-        }]);
-
-    }
-
-
 }
